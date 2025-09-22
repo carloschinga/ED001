eddad1eed6a1c47e0d6bc88f859c1142141358a5
carloschinga <chinga_carlos@hotmail.com>
2025-09-22 14:21:20 -0500

    Primer Ejercicio modificado

--- src/main/java/com/mycompany/ed001/ED001.java
+++ src/main/java/com/mycompany/ed001/ED001.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.ed001;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author USER
@@ -11,16 +13,20 @@ package com.mycompany.ed001;
 public class ED001 {
 
     public static void main(String[] args) {
-        int[] codigo= new int[10];
-        String[] nombres=new String[10];
-        for (int i = 0; i < 10; i++) {
+        int[] codigo= new int[5];
+        String[] nombre=new String[5];    
+        double[] monto= new double[5];
+        for (int i = 0; i < 5; i++) {
             codigo[i]= (i+1);
+            nombre[i]= JOptionPane.
+                    showInputDialog("Ingrese nombre " + (i+1)+":");
+            monto[i]=Double.parseDouble(
+                    JOptionPane.
+                            showInputDialog("Ingrese monto " + (i+1)+":")
+                        );                    
         }
-        
-        for (int i = 0; i < 10; i++) {
-            System.out.println(codigo[i]);
-            
-            
+        for (int i = 0; i < 5; i++) {
+            System.out.println(codigo[i] + " - "+ nombre[i] + " - " + monto[i]);            
         }        
     }
 }
