16da3772332954ecfe586c718d6df3e79eab294f
Luxu <valladaresdiego006@gmail.com>
2025-09-22 14:02:02 -0500

    Creacion de nombres

--- src/main/java/com/mycompany/ed001/ED001.java
+++ src/main/java/com/mycompany/ed001/ED001.java
@@ -12,13 +12,15 @@ public class ED001 {
 
     public static void main(String[] args) {
         int[] codigo= new int[10];
-        
+        String[] nombres=new String[10];
         for (int i = 0; i < 10; i++) {
             codigo[i]= (i+1);
         }
         
         for (int i = 0; i < 10; i++) {
             System.out.println(codigo[i]);
+            
+            
         }        
     }
 }
